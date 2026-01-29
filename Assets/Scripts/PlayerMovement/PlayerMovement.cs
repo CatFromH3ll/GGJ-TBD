86c85890db9d4bb4dac6594ce8511a9f98b1864a
MaximAbramov <maximab2002@gmail.com>
2026-01-29 23:59:54 +0200

    update player movement v0,5

--- Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -1,52 +1,84 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Variables visible in the Unity Inspector
-    public float speed = 4.0f;       // How fast the player moves left/right
-    public float jumpForce = 6.0f;   // The strength of the upward "kick"
+    public float speed = 4.0f;       // player speed
+    public float jumpForce = 6.0f;   // player jump force
+    
+    public GameObject GravityMask;
+    private bool isGravityFlipped = false;
     
     // Internal variables
-    private Rigidbody2D rb;          // Reference to the Physics component
-    private bool isGrounded;         // A "toggle" to know if we are on the floor
+    private Rigidbody2D rb;    // rb stands for rigidbody 
+    private bool isGrounded;   // A "toggle" to know if we are on the floor
 
     // Awake is called when the script instance is being loaded
     void Awake()
     {
-        // Link our 'rb' variable to the actual Rigidbody2D component on this object
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();// get the component of the rigidbody
+        
+        if (GravityMask != null) //if the mask not used mean the gravity is normal
+        {
+            GravityMask.SetActive(false);
+        }
     }
 
     // Update is called once every frame (handling Input)
     void Update()
     {
-        // 1. HORIZONTAL MOVEMENT
-        // Get input from A/D or Left/Right arrows (-1 to 1)
+        if (isGrounded)
+        {
+            if (Input.GetButtonDown("GravityMask"))//first mask input
+            {
+                ToggleGravity();
+                isGrounded = false;
+            }
+            else
+            {
+                //Debug.Log("you can use the mask only when you are on the ground");
+            }
+        }
+ 
         float xInput = Input.GetAxis("Horizontal");
-        
-        // Create a Vector3 to store our target velocity
-        // x = input * speed
-        // y = we keep the current vertical velocity (so gravity still works!)
-        // z = 0 (since it's a 2D game)
         Vector3 moveDirection = new Vector3(xInput * speed, rb.linearVelocity.y, 0f);
-        
-        // Set the physics velocity to our new Vector3
-        // Note: Unity automatically converts Vector3 to Vector2 here by ignoring Z
         rb.linearVelocity = moveDirection;
 
         // 2. JUMPING
-        // Check if the "Jump" key is pressed (Space) AND the player is on the ground
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown("PlayerJump") && isGrounded)
         {
+            float jumpDirection = isGravityFlipped ? -1f : 1f;
             // Vector3.up is shorthand for (0, 1, 0)
             // ForceMode2D.Impulse makes the force happen instantly (perfect for jumps)
-            rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
-            
+            rb.AddForce(Vector3.up * jumpForce * jumpDirection, ForceMode2D.Impulse);
             // Set grounded to false so we can't jump again while in mid-air
             isGrounded = false;
         }
     }
 
+    void ToggleGravity()
+    {
+        isGravityFlipped = !isGravityFlipped;
+        rb.gravityScale = rb.gravityScale * -1;
+        if (isGravityFlipped)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 180f);
+            if (GravityMask != null)
+            {
+                GravityMask.SetActive(true);
+            }
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0f);
+            if (GravityMask != null)
+            {
+                GravityMask.SetActive(false);
+            }
+        }
+    }
+    
     // This function runs automatically when the player's collider hits another collider
     private void OnCollisionEnter2D(Collision2D collision)
     {
