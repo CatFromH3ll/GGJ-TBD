bc6e0e25aa6a480110ce82029af0516eac4003fc
CatFromH3ll <140316561+CatFromH3ll@users.noreply.github.com>
2026-01-31 14:40:16 +0200

    Complit build (no sound nor anim)

---
+++ Assets/levelTransition.cs
@@ -0,0 +1,26 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelTransition : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") ;
+        {
+            SceneManager.LoadScene("Room1");
+        }
+    }
+}
